0f8709ded49d2cc93d2f93aadf6c9fea3394c605
BogdanGabriel05 <bogdan0511@Xubuntu.myguest.virtualbox.org>
2024-09-24 17:08:37 +0300

    Remade the function that asks an input
    Tha function now asks a number between 1, 2 or anything else.

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ getComputerChoice();
 // Human's choice code
 function getHumanChoice() {
     let humanChoice = prompt("Enter your choice!")
-    // Made the user input case sensitive
-    let firstLetter = humanChoice.slice(0, 1).toUpperCase();
-    let restOfWord = humanChoice.slice(1).toLowerCase();
-    let wholeWord = firstLetter + restOfWord;
-    return wholeWord;
+
+    if (humanChoice === "1") {
+        return "Rock"
+    } else if (humanChoice === "2") {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+
+
 
 }
 getHumanChoice();
@@ -30,6 +35,62 @@ const computerScore = 0;
 // Game round
 function playRound(humanChoice, computerChoice) {
 
+
+
+if (humanSelection === "Rock" && computerSelection === "Scissors") {
+    console.log("You lose! Rock beats scissors") 
+} else if (computerSelection === "Rock" && humanSelection === "Scissors") {
+    console.log("You lose! Rock beats scissors.")
+} else if (humanSelection === "Paper" && computerSelection === "Rock") {
+    console.log("You lose! Paper beats rock.") 
+} else if (computerSelection === "Paper" && humanSelection === "Rock") {
+    console.log("You lose! Paper beats rock.")
+} else if (humanSelection === "Scissors" && computerSelection === "Paper") {
+    console.log("You lose! Scissors beats paper.")
+} else if(computerSelection === "Scissors" && humanSelection === "Paper") {
+    console.log("You lose! Scissors beats paper.")
+}  
+
+
+
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(); 
 
+playRound(humanSelection, computerSelection);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Made the user input case sensitive
+// let firstLetter = humanChoice.slice(0, 1).toUpperCase();
+// let restOfWord = humanChoice.slice(1).toLowerCase();
+// let wholeWord = firstLetter + restOfWord;
+// return wholeWord;
