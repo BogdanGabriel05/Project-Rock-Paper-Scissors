117dc2ccae4b8448375e93010ab362109396833e
BogdanGabriel05 <bogdan0511@Xubuntu.myguest.virtualbox.org>
2024-09-24 12:19:38 +0300

    Generated the human number
    I created the prompt that will ask the "human player" for an imput and will return it.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-    function getComputerChoice() {
-        let randomNumber = Math.floor((Math.random() * 3) + 1);
-        if (randomNumber === 1) {
-            return "Rock"
-        } else if (randomNumber === 2) {
-            return "Paper"
-        } else {
-            return "Scissors"
-        }
+// Computer's choice code
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    if (randomNumber === 1) {
+        return "Rock"
+    } else if (randomNumber === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
-
 getComputerChoice();
+
+// Human's choice code
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice!")
+    return humanChoice;
+}
+getHumanChoice();
 
 
